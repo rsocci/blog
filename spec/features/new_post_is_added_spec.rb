bd9791de9d7bc129a015aa1cd8a519d4f9f90721
Romina Vargas <rsocci@me.com>
2013-09-07 18:05:54 -0400

    Integration tests for adding new post, Post controller actions (index,show,new,create), Post views

---
+++ spec/features/new_post_is_added_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'new post is added' do
+  before :each do
+    Post.create(title: 'My post', content: 'The content', author: 'Romina')
+    visit root_path
+  end
+  
+  scenario 'new post appears on posts list' do
+    expect(page).to have_content('My post')
+  end
+  
+  scenario 'user wants to see a specific post' do
+    click_link('My post')
+    expect(page).to have_no_selector('table')
+    expect(page).to have_content('My post')
+    expect(page).to have_content('The content')
+  end
+  
+  scenario 'user wants to create new post' do
+    click_button('New Post')
+    fill_in 'Title', with: 'Hello World'
+    fill_in 'Content', with: 'Just saying hello'
+    fill_in 'Author', with: 'Romina Vargas'
+    click_button('Submit')
+  end
+end

fe3ea076bab9b4f9b978629a923e79a9eb22763b
Romina Vargas <rsocci@me.com>
2013-09-09 16:54:46 -0400

    Add comments resource and ability to add comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(params[:post_id])
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end

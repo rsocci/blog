3d51c85a4e4a904e2cf2e63abec58955bc1baf76
Romina Vargas <rsocci@me.com>
2013-09-10 19:16:16 -0400

    modify edit test to visit root_path

--- spec/features/post_is_edited_spec.rb
+++ spec/features/post_is_edited_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 feature 'post is edited' do
   before :each do
     @post = Post.create(title: 'My post', content: 'The content', author: 'Romina')
-    visit post_path(@post.id)
+    visit root_path
   end
   
   scenario 'user sees edited post' do
-    click_button('Edit')
+    click_link('Edit')
     page.fill_in 'Title', with: 'Welcome'
     click_button('Submit')
     expect(page).to have_content('Post was updated successfully')
